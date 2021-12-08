53307986a1dfd9057ea85615d3cbb80b7733d2fa
Salih Gökhan Topçu <gtopcu@gmail.com;2D>
2021-12-08 15:08:02 +0300

    api gw samples

--- Lambda/_Samples/_initial.py
+++ Lambda/_Samples/_initial.py
@@ -1,7 +1,18 @@
-import json
-def lambda_handler(event, context):
-    print("In lambda handler")
 
+import json
+
+def lambda_handler(event, context):
+
+    print("In lambda handler")
+    print(event)
+
+    # Parse Query String
+    queryParam = event["queryStringParameters"]["param1"]
+    print(queryParam)
+
+    # Parse Request Body
+    orderID = event["requestParameters"]["orderID"]
+    
     resp = {
         "statusCode": 200,
         "headers": {
