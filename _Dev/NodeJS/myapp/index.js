1258b877d9395067fc402d3136de92d9fa6ecc13
System Administrator <gtopcu@gmail.com;2D>
2021-12-31 18:31:07 +0300

    nodeJS-NPM-Yarn notes

--- _Dev/NodeJS/myapp/index.js
+++ _Dev/NodeJS/myapp/index.js
@@ -1,22 +1,8 @@
 
-/* Hello World! program in Node.js */
-console.log("Hello World!");
+//node index.js
+//nodemon index.js
 
-var http = require("http");
-http.createServer(function (request, response) {
-    
-    // Log the request
-    console.log('Request received');
+var myDate = new Date();
 
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-
- }).listen(8081);
- 
- // Console will print the message
- console.log('Server running at http://127.0.0.1:8081/');
+console.log(myDate);
+console.log("hello");
---
+++ _Dev/NodeJS/sampleNPM/index.js
@@ -0,0 +1,30 @@
+
+/*
+npm -v
+npm install -g nodemon (live output)
+npm i moment (datetime pretty print - momentjs.com)
+
+npm init
+nodemon index.js
+
+npm i -g browserify
+npm i uniq
+
+browserify index.js -o bundle.js
+*/
+
+var author = "hukanege";
+var myDate = new Date();
+
+console.log(author);
+console.log(myDate);
+
+var moment = require('moment');
+console.log(moment(myDate).format());
+console.log(moment(myDate).format("YYYY-MM-DD"));
+console.log(moment(myDate).format("LL"));
+
+var myArray = [1, 2, 2, 3, 3, 3];
+var unique = require("uniq");
+console.log(unique());
+
