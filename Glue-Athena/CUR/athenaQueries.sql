df52b4f4098753e617a1f5dc6878a3f5ac3984b0
S. Gökhan Topçu <gtopcu@gmail.com>
2022-04-12 16:28:01 +0300

    updates

---
+++ Glue-Athena/CUR/athenaQueries.sql
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=KEeJEZTYE8E
+
+SELECT * FROM "athenacurcfn_all_daily_parquet"."cost_and_usage_data_status" limit 10;
+
+SELECT * FROM "athenacurcfn_all_daily_parquet"."all_daily_parquet" limit 10;
