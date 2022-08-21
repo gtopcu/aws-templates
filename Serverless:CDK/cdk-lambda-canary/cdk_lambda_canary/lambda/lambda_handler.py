83c6fa6248ed2637f27c81ae6f89eac69b9e5fc0
S. Gökhan Topçu <gtopcu@gmail.com>
2022-08-21 22:08:13 +0300

    updates

--- Serverless:CDK/cdk-lambda-canary/cdk_lambda_canary/lambda/lambda_handler.py
+++ Serverless:CDK/cdk-lambda-canary/cdk_lambda_canary/lambda/lambda_handler.py
@@ -1,7 +1,24 @@
+import os
+import sys
+import logging
 import json
+import requests
+import uuid
+import time
+import datetime
+import random
+
+logger = logging.getLogger()
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from CDK!')
-    }
+
+    logger.info("Starting lambda handler, env: " + os.environ("env") + " path: " + os.path())
+    try:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from CDK!')
+        }
+        logger.info("Lambda handler done")
+    except KeyError as e:
+        logger.error("Error occured, details: %s", "lambda error", exc_info=1)
+        raise e
