d0413a745e27b94dda83c8965d51a082c780ddca
S. Gokhan Topcu <gtopcu@gmail.com>
2023-09-24 16:06:27 +0300

    updates

--- boto3/CodeWhisperer/cwTests.py
+++ boto3/CodeWhisperer/cwTests.py
@@ -1,18 +1,33 @@
+# Code whisperer: Option + C
 
 import boto3
 
 def upload_to_S3(local_file, bucket, s3_file):
-    """
-    Uploads a file to an S3 bucket
-    """
     s3 = boto3.client('s3')
     try:
         s3.upload_file(local_file, bucket, s3_file)
         print("Upload Successful")
         return True
-    except FileNotFoundError:
-        print("The file was not found")
+    except Exception as e:
+        print(f"Error: {e}")
         return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
+
+# upload file to s3 bucket
+def uploadToS3(bucketName, fileName, filePath):
+    s3 = boto3.client('s3')
+    s3.upload_file(filePath, bucketName, fileName)
+
+# write to dynamodb
+def writeToDynamo(tableName, item):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(tableName)
+    table.put_item(Item=item)
+
+# read from dynamodb
+def readFromDynamo(tableName, key):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(tableName)
+    response = table.get_item(Key=key)
+    return response
+
+
