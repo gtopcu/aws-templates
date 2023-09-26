1295f6044d11b8e6a5fe917553c01d072066e868
S. Gokhan Topcu <gtopcu@gmail.com>
2023-09-26 12:09:26 +0300

    updates

--- boto3-projecttemplate/aws_utils/s3_utils.py
+++ boto3-projecttemplate/aws_utils/s3_utils.py
@@ -3,14 +3,11 @@ import boto3
 
 s3 = boto3.client('s3')
 
-def uploadToS3(local_file, bucket, s3_file):
+def uploadToS3(local_file, bucket, s3_file) -> None:
     try:
         s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
-        return True
     except Exception as e:
-        print(f"Error: {e}")
-        return False
+        raise Exception(f"S3 Upload Error: {e}")
 
 """
 # upload file to s3 bucket
@@ -18,3 +15,9 @@ def uploadToS3(bucketName, fileName, filePath):
     s3 = boto3.client('s3')
     s3.upload_file(filePath, bucketName, fileName)
 """
+
+def downloadFromS3(bucketName, fileName, filePath) -> None:
+    try:
+        s3.download_file(bucketName, fileName, filePath)
+    except Exception as e:
+        raise(f"S3 Download Error: {e}")
