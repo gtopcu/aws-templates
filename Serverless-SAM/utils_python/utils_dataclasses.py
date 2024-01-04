95cc75e68edccd3f59341be91376f08dd26064b8
S. Gokhan Topcu <gtopcu@gmail.com>
2024-01-04 23:20:06 +0300

    updates

--- Serverless-SAM/utils_python/utils_dataclasses.py
+++ Serverless-SAM/utils_python/utils_dataclasses.py
@@ -1,16 +1,21 @@
+import dataclasses
 from dataclasses import dataclass, field, astuple, asdict
 from datetime import datetime
 
-@dataclass(frozen=True, order=True)
+@dataclass(frozen=True #generates __hash__
+           order=True) #generates __eq__ __lt__ __gt__ etc
 class Employee:
     id: int
-    name: str = field(default="GT")
+    #id2 : int = field()
+    name: str 
+    #name2: str = field(default="GT")
     date: datetime
     addresses: list[str] = field(default_factory=list, compare=False, hash=False, repr=False)
 
 if __name__ == '__main__':
-    emp = Employee(1, 'John')
-    print(emp)
-    print(astuple(emp))
-    print(asdict(emp))
+    emp1 = Employee(1, 'John')
+    print(emp1)
+    print(astuple(emp1))
+    print(asdict(emp1))
+    emp2 = dataclasses.replace(emp1, id=2)
 
