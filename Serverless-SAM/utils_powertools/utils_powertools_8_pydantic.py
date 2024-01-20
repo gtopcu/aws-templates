f8261d813db44c4bb4467977f29aba8e22cc372a
S. Gokhan Topcu <gtopcu@gmail.com>
2024-01-20 18:45:15 +0300

    updates

--- Serverless-SAM/utils_powertools/utils_powertools_8_pydantic.py
+++ Serverless-SAM/utils_powertools/utils_powertools_8_pydantic.py
@@ -1,5 +1,7 @@
 # https://docs.powertools.aws.dev/lambda/python/2.29.1/utilities/parser/#event_parser-decorator
-from aws_lambda_powertools.utilities.parser import event_parser, BaseModel
+# https://github.com/koxudaxi/datamodel-code-generator
+
+from aws_lambda_powertools.utilities.parser import event_parser, BaseModel, parse, validator, root_validator
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from typing import List, Optional
 
@@ -10,12 +12,35 @@ class OrderItem(BaseModel):
     quantity: int
     description: str
 
+    @validator('description')
+    def check_desc(cls, v):
+        if v != "hello world":
+            raise ValueError("Description must be hello world!")
+        return v
+    
+    @validator('*')
+    def has_whitespace(cls, v):
+        if v is None:
+            raise ValueError("All fields must have values")
+        return v    
+
 class Order(BaseModel):
     id: int
     description: str
     items: List[OrderItem] # nesting models are supported
     optional_field: Optional[str] = None # this field may or may not be available when parsing
 
+class UserModel(BaseModel):
+    username: str
+    password1: str
+    password2: str
+
+    @root_validator
+    def check_passwords_match(cls, values):
+        pw1, pw2 = values.get('password1'), values.get('password2')
+        if pw1 is not None and pw2 is not None and pw1 != pw2:
+            raise ValueError('passwords do not match')
+        return values
 
 @event_parser(model=Order)
 def handler(event: Order, context: LambdaContext):
