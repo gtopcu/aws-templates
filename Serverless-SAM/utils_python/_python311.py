cceafeb16c3a10bd1e1e0feb7d8a93172e773b62
S. Gokhan Topcu <gtopcu@gmail.com>
2024-02-17 11:23:48 +0300

    updates

--- Serverless-SAM/utils_python/_python311.py
+++ Serverless-SAM/utils_python/_python311.py
@@ -1,22 +1,34 @@
 
+####################################################################################################
+# 1 - Multiple Except
+
+# def main() -> None:
+#     try:
+#         x = 0 / 3
+#         print("Division: " + x)
+#     except* (KeyError, ValueError) as e:
+#        print(e)
+
+####################################################################################################
+# 2 - asyncio TaskGroups instead of gather
+
 import asyncio
 
-def main() -> None:
-    try:
-        x = 0 / 3
-        print("Division: " + x)
-    except* (KeyError, ValueError) as e:
-       print(e)
+async def asyncfunc(number: int) -> None:
+    await asyncio.sleep(number)
+    return number
 
-async def asyncmain() -> None:
+async def main() -> None:
     async with asyncio.TaskGroup as tg:
-        task1 = tg.create_task(asyncio.sleep(1))
+        tg.create_task(asyncfunc(1))
         print(task1)
-        task2 = tg.create_task(asyncio.sleep(2))
-        print(task2)
+    #     task2 = tg.create_task(asyncfunc(2))
+    #     print(task2)
     print("Both tasks have been completed")
-
     
-if __name__ == "__main__":
-    main()
-    #asyncio.run(asyncmain())
+asyncio.run(main())
+# if __name__ == "__main__":
+#     #main()
+    
+
+####################################################################################################
--- Serverless-SAM/utils_python/test.py
+++ Serverless-SAM/utils_python/test.py
@@ -5,53 +5,35 @@ import datetime
 import json
 from pathlib import Path
 import pathlib
-import pydantic
-from pydantic import BaseModel
 import typing
-from typing import Set, List, Dict, Union, Optional, Any #not necessary after Python 3.9
-
-
-# list | None
-# str | int
-def getData(default: Any, key: Optional[dict] = None):
-    pass
-
-def myUnion(input: Union[str, int]) -> None:
-    print(input)
-
-def myUtil(mylist: str | None=None) -> None:
-    return mylist
-
-def time_this() -> None:
-    time.sleep(2)
-    for i in range(100000):
-        i += 1
 
 def main() -> None:
-    print(os.getcwd())
-    print(os.listdir('.'))
-    print(Path().absolute())
-    #print(datetime.datetime.now())
-    #print(dir(int))
-    #print(__file__)
-    #print(pathlib.Path(__file__).parent / "examples")
+    # print(os.getcwd())
+    # print(os.listdir('.'))
+    # print(Path().absolute())
+    # print(datetime.datetime.now())
+    # print(dir(int))
+    # print(__file__)
+    # print(pathlib.Path(__file__).parent / "examples")
     x, y = 3, 4
-    print(x, y)
-    nums = [x for x in range(10) if x % 2 == 0]
-    print(nums)
-    print(typing.TYPE_CHECKING)
+    # print(x, y)
+    # print(nums)
 
     #total_time = timeit(time_this())
     #print(f"Total time: {total_time:.3f}")    
 
-    char = "c"
-    match char:
-        case "a":
-            print("a")
-        case "b":
-            print("b")
-        case _:
-            print("default")
+    # char = "c"
+    # match char:
+    #     case "a":
+    #         print("a")
+    #     case "b":
+    #         print("b")
+    #     case _:
+    #         print("default")
+
+    # nums = [x for x in range(10) if x % 2 == 0]
+
+    print(chr(10))
 
 if __name__ == "__main__":
     main()
