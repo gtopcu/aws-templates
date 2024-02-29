1aa9acf9eb7897ff6432ad4f987f22738e5171af
S. Gokhan Topcu <gtopcu@gmail.com>
2024-03-01 00:36:17 +0300

    updates

--- Serverless-SAM/utils_python/utils_pandasnumpy.py
+++ Serverless-SAM/utils_python/utils_pandasnumpy.py
@@ -1,1 +1,14 @@
 # https://www.youtube.com/watch?v=iGFdh6_FePU
+
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+# print(np.array([0, 6]).mean())
+# speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+# print(np.mean(speed))
+# print(np.median(speed))
+# print(stats.mode(speed))
+
+df = pd.read_csv("data.csv")
+
