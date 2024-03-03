cc7604a5ac107582335a66a1641dc6626e05f0d7
S. Gokhan Topcu <gtopcu@gmail.com>
2024-03-03 22:00:04 +0300

    updates

--- Serverless-SAM/utils_python/test.py
+++ Serverless-SAM/utils_python/test.py
@@ -1,18 +1,17 @@
+# from collections import deque, defaultdict, namedtuple
+# #import frozenset
+# import datetime
+# import time
+# import os
 
-from collections import deque, defaultdict, namedtuple
-#import frozenset
-import datetime
-import time
-import os
+# BIG_CONSTANT: int = 10000000
 
-BIG_CONSTANT: int = 10000000
 
 def main() -> None:
-    
     # x, y = 3, 4
     # print(x, y)
-    
-    #print(f"Total time: {total_time:.3f}")    
+
+    # print(f"Total time: {total_time:.3f}")
 
     # char = "c"
     # match char:
@@ -25,21 +24,37 @@ def main() -> None:
 
     # print(chr(100))
 
-    my_list = [x for x in range(10) if x % 2 == 0]
-    my_set = {i for i in range(10)}
-    my_set2 = set()
-    # my_frozenset = frozenset(my_list)
-    my_dict = {x: x ** 2 for x in range(10)}
-    my_deque = deque(my_list, maxlen=5)
-    print(my_deque)
-    my_defaultdict = defaultdict(lambda: "default")    
-    my_defaultdict = defaultdict(int)    
-    print(my_defaultdict["NoKeyError"])
+    # my_list = [x for x in range(10) if x % 2 == 0]
+    # my_set = {i for i in range(10)}
+    # my_set2 = set()
+    # # my_frozenset = frozenset(my_list)
+    # my_dict = {x: x ** 2 for x in range(10)}
+    # my_deque = deque(my_list, maxlen=5)
+    # print(my_deque)
+    # my_defaultdict = defaultdict(lambda: "default")
+    # my_defaultdict = defaultdict(int)
+    # print(my_defaultdict["NoKeyError"])
 
-    print( (12 * 3) if True else 6 )    
-    #print(time.time())
-    print(BIG_CONSTANT)
-    print("Done")
+    # print( (12 * 3) if True else 6 )
+    # #print(time.time())
+    # print(BIG_CONSTANT)
+    # print("Done")
+
+    # list = [x for x in range(0, 10, 2)]
+    # list2 = [x*x for x in range(0, 10) if x%2==1]
+    # print(list.count(2))
+    # print(list.index(1))
+    # print(list.extend(list2))
+    # print(list.pop(0))
+    # print(list.remove(2))
+    # print(list.reverse())
+    # list.sort(reverse=True)
+    # print(list)
+
+    dict = {x: x**2 for x in range(0, 10)}
+    print(dict)
+    print()
+
 
 if __name__ == "__main__":
     main()
