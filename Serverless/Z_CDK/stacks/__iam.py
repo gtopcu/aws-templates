ae67a84d6ca5bda76df05538e6a4a3541281c729
S. Gökhan Topçu <gtopcu@gmail.com>
2024-12-30 17:55:51 +0300

    added CDK IAM

--- Serverless/Z_CDK/stacks/__iam.py
+++ Serverless/Z_CDK/stacks/__iam.py
@@ -14,12 +14,44 @@ class LambdaStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
-        
-        # Create Lambda execution role
         lambda_role = iam.Role(
             self, "LambdaExecutionRole",
             assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
             managed_policies=[
                 iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
+            ]
+        )
+
+        ec2_role = iam.Role(
+            self, "EC2Role",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMPatchAssociation"),
+            ],
+            inline_policies=ec2_policy
+        )
+
+        ec2_policy = iam.Policy( self, "EC2Policy",
+            roles=[ec2_role],
+            policy_name="EC2Policy",
+            # document=ssm_policy_document
+            # statements=ssm_policy_document.statements
+        )
+
+        ssm_policy_document = iam.PolicyDocument(
+            statements=[
+                iam.PolicyStatement(
+                    actions=[
+                        "ssm:UpdateInstanceInformation",
+                        "ssmmessages:CreateControlChannel",
+                        "ssmmessages:CreateDataChannel",
+                        "ssmmessages:OpenControlChannel",
+                        "ssmmessages:OpenDataChannel"
+                    ],
+                    resources=["*"],
+                    effect=iam.Effect.ALLOW
+                )
             ]
         )
