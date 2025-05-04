ea64744dd1e157aa783b0b7916099c2deec4105b
S. Gökhan Topçu <gtopcu@gmail.com>
2025-05-04 23:21:26 +0300

    fix

--- Serverless/utils_python/_aa.py
+++ Serverless/utils_python/_aa.py
@@ -2,8 +2,11 @@
 from typing import Self, Any, Optional, Final, Literal, NewType, TypeAlias, TypedDict
 from collections import namedtuple, deque, OrderedDict, defaultdict, ChainMap
 
-import logging
+class MyClass(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
 
+import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.info("Logging is set up.")
@@ -19,10 +22,10 @@ time.perf_counter()
 # raise SystemError("")
 
 # id len type isinstance repr
-# iter next  
+# iter next 
 # any all chr ord 
 # abs round sum min max pow
-# sorted reversed filter map reduce zip 
+# sorted reversed filter map reduce zip enumerate
 # getattr delattr setattr 
 
 my_list: list[int] = [1, 2, 3]
@@ -30,6 +33,12 @@ my_tuple: tuple[int, str] = (1, "Hello")
 my_dict: dict[str, int] = {"one": 1, "two": 2}
 my_set: set[int] = {1, 2, 3}
 my_string: str = "Hello, World!"
+
+my_list2 = [i for i in range(0,100,10) if i%5==0]
+my_list3 = [*range(5)]
+print(my_list3)
+my_list4 = list(range(5))
+print(my_list4)
 
 def nasty(val=[]):
     val.append(1)
@@ -45,6 +54,18 @@ if my_string is None or my_list is not None:
 if 1 == 1:
     print("1 == 1")
 
+def add_wrapper(func):
+    def wrapper(*args, **kwargs):
+        print("Before function call")
+        result = func(*args, **kwargs)
+        print("After function call")
+        return result
+    return wrapper
+
+@add_wrapper
+def add_numbers(a: int, b: int) -> int: # Callable[[int, int], int]
+    print(f"Adding {a} and {b}")
+    return a + b
 
 def main() -> None:
     print("{}".format(my_string))
@@ -55,45 +76,21 @@ def main() -> None:
 
     for i in "123456":
         print(i, end="-")
+    print("\n")
+        
+    add_numbers(1, 2)
 
 if __name__ == "__main__":
     main()
 
 
-def add_numbers(a: int, b: int) -> int:
-    """
-    Add two numbers together.
-    
-    Args:
-        a (int): The first number.
-        b (int): The second number.
-        
-    Returns:
-        int: The sum of the two numbers.
-    """
-    return a + b
-
-def subtract_numbers(a: int, b: int) -> int:
-    """
-    Subtract the second number from the first.
-    
-    Args:
-        a (int): The first number.
-        b (int): The second number.
-        
-    Returns:
-        int: The result of subtracting b from a.
-    """
-    return a - b  
-
-
 """ 
 import pytest
 
-def test_subtract_numbers_positive():
-    assert subtract_numbers(5, 3) == 2
+def test_add_numbers():
+    assert add_numbers(-1, 3) == 2
 
-def test_subtract_numbers_type_error():
+def test_add_numbers_type_error():
     with pytest.raises(TypeError):
-        subtract_numbers("5", 3) 
+        add_numbers("5", 3) 
 """
