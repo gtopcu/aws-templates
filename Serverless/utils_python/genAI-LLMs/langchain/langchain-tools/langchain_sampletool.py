864e0fefe50ba4751ec509d076877b4883d3d206
S. Gokhan Topcu <gtopcu@gmail.com>
2025-05-17 15:35:29 +0300

    langchain tools

---
+++ Serverless/utils_python/genAI-LLMs/langchain/langchain-tools/langchain_sampletool.py
@@ -0,0 +1,19 @@
+
+# pip install langchain-tools
+
+from langchain_tools import tool
+
+class CalculatorTools:
+
+    @tool("Make a calculator")
+    def calculator(operation):
+        """
+        Useful to perform any mathematical calculation
+        like sum, minus, multiplication, division etc.
+        The input to this tool should be a mathematical
+        expression, like 20*2 or 3/3+10
+        """
+        try:
+            return eval(operation)
+        except SyntaxError:
+            return "Error, invalid syntax in the operation"
