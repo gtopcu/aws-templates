0be6b4dce1b500b26d9bbac8a24006c1f53b0f74
S. Gokhan Topcu <gtopcu@gmail.com>
2025-05-18 16:08:28 +0300

    deepseek langchain ollama

---
+++ Serverless/utils_python/genAI-LLMs/langchain/langchain_deepseekOllama_textTosql.py
@@ -0,0 +1,6 @@
+
+# https://www.youtube.com/watch?v=kem-v9MXuG4
+# pip install -U -q streamlit, langchain-core, langchain-community, langchain-ollama, ollama, SQLAlchemy
+
+# ollama pull deepseek-r1:8b #14b
+
