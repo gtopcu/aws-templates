1da1d02f8935b50aa8687788b6f20bd0f6159bdf
S. Gokhan Topcu <gtopcu@gmail.com>
2025-05-23 00:14:48 +0300

    cleanup

--- ZAWS_cdk-python-cookiecutter/{{cookiecutter.repo_name}}/README.md
+++
@@ -1,6 +0,0 @@
-# {{cookiecutter.repo_name}}
-This is a great README file by {{cookiecutter.author}} ({{cookiecutter.email}})
-
-
-The service is about:
-{{cookiecutter.description}}
--- ZAWS_cdk-python-cookiecutter/{{cookiecutter.repo_name}}/{{cookiecutter.service_name}}/dal/__init__.py
+++
@@ -1,9 +0,0 @@
-from functools import lru_cache
-
-from {{cookiecutter.service_name}}.dal.db_handler import DalHandler
-from {{cookiecutter.service_name}}.dal.dynamo_dal_handler import DynamoDalHandler
-
-
-@lru_cache
-def get_dal_handler(table_name: str) -> DalHandler:
-    return DynamoDalHandler(table_name)
