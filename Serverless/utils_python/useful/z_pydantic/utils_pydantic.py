214177f962323d334428479ea6a7e71be4fbcbd3
S. Gokhan Topcu <gtopcu@gmail.com>
2025-06-02 12:13:26 +0300

    pydantic decimal

--- Serverless/utils_python/useful/z_pydantic/utils_pydantic.py
+++ Serverless/utils_python/useful/z_pydantic/utils_pydantic.py
@@ -95,7 +95,7 @@ from pydantic import (
 # https://github.com/boto/boto3/issues/665#issuecomment-340260257
 # from decimal import Decimal, getcontext, setcontext, ExtendedContext
 # setcontext(ExtendedContext)
-getcontext().prec = 2
+# getcontext().prec = 2
 # round(Decimal(1.2345), 2)
 
 # https://github.com/pydantic/pydantic/issues/8006
