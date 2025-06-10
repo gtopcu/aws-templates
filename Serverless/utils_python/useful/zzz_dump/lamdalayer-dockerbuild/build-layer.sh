91ef80f36ae0469e157d0b627692825acca83a1b
S. Gokhan Topcu <gtopcu@gmail.com>
2025-06-10 14:33:24 +0300

    notebookLM init

--- Serverless/utils_python/useful/zzz_dump/lamdalayer-dockerbuild/build-layer.sh
+++ Serverless/utils_python/useful/zzz_dump/lamdalayer-dockerbuild/build-layer.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# This script creates the common lambda layer as a zip, specifically for the
-# x86_64 variant of lambdas. These must then be uploaded to AWS as the lambda layer
-# secr-py-common-dependencies-layer
-# variables.tf must then be updated with the new version arn
 
+# Creates common lambda layer as a zip file for x86_64 lambdas
 
 rm -rf .build && mkdir .build
 rm common-lambda-layer.zip
