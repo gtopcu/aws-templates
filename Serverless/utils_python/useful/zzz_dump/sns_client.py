7bf30030e445dd1cd8b1ba6b13bc8ecc3ebc9218
S. Gokhan Topcu <gtopcu@gmail.com>
2025-06-20 14:16:23 +0300

    updated sns wrapper

--- Serverless/utils_python/useful/zzz_dump/sns_client.py
+++ Serverless/utils_python/useful/zzz_dump/sns_client.py
@@ -1,29 +1,23 @@
 
 import json
-from functools import lru_cache
-from pydantic import BaseModel
 
 import boto3
 from aws_lambda_powertools import Logger
-
-# pip install mypy_boto3_sns
 from mypy_boto3_sns import Client
-
+from pydantic import BaseModel
 
 logger = Logger()
-
+sns_client:Client = boto3.client("sns")
 
 class SnsWrapper:
     """Encapsulates Amazon SNS topic functions."""
 
-    __sns_client: Client
-
     def __init__(self):
-        self.__sns_client:Client = boto3.client("sns")
+        self.__sns_client = sns_client
 
     def publish(self, target: str, request: BaseModel) -> str:
         """
-        Publish request to target arn.
+        Publishes the request to the SNS topic with the target arn.
         Request must be a Pydantic model which will be serialized to the message.
 
         :return str: MessageId for the message
@@ -42,8 +36,3 @@ class SnsWrapper:
                 f"Published message {response['MessageId']} to topic arn: {target}"
             )
             return response["MessageId"]
-
-
-@lru_cache(maxsize=1)
-def get_sns_wrapper():
-    return SnsWrapper()
