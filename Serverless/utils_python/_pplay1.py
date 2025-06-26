0ead963dd72bfebaa671ea84bc9d3fcc4e84a274
S. Gokhan Topcu <gtopcu@gmail.com>
2025-06-26 14:13:21 +0300

    orjson

--- Serverless/utils_python/_pplay1.py
+++ Serverless/utils_python/_pplay1.py
@@ -599,13 +599,19 @@ from functools import reduce, partial, cache, lru_cache, cmp_to_key, singledispa
 # getcontext().prec = 3
 # my_decimal = Decimal(1)
 # print(my_decimal / Decimal(3))
+# my_dict = json.loads('{"name": "John", "age": 30, "money": 2.75}', parse_float=Decimal)
+
+# return { statusCode: 200, body: "Hello, World!" }
+# print(json.dumps({"statusCode": 200, "body": "Hello, World!"}, sort_keys=True, indent=4, separators=(",", ": "), ensure_ascii=False))
+# print(json.dumps(my_dict, default=str))
+# with open("file.json", "r") as f:
+#     data:dict[str, Any] = json.load(f)
 
 # -------------------------------------------------------------------------------------------------
 
-# import boto3
-
 # kwargs.update({"ExclusiveStartKey": response["LastEvaluatedKey"]})
 
+# import boto3
 # from botocore.exceptions import ClientError, ConditionCheckFailedException
 # client = boto3.client(service_name="dynamodb", region_name="us-east-1")
 # def lambda_handler(event, context):    
@@ -621,19 +627,6 @@ from functools import reduce, partial, cache, lru_cache, cmp_to_key, singledispa
 #         print("Error Code: " + f"{err.response['Error']['Code']}")
 #         print("Error Message: " + f"{err.response['Error']['Message']}")
 #         raise err
-
-# -------------------------------------------------------------------------------------------------
-
-# import json
-# from decimal import Decimal
-# # return { statusCode: 200, body: "Hello, World!" }
-# print(json.dumps({"statusCode": 200, "body": "Hello, World!"}, sort_keys=True, indent=4, separators=(",", ": "), ensure_ascii=False))
-# my_dict = json.loads('{"name": "John", "age": 30, "money": 2.75}', parse_float=Decimal)
-# print(my_dict)
-# print(json.dumps(my_dict, default=str))
-
-# with open("file.json", "r") as f:
-#     data:dict[str, Any] = json.load(f)
 
 # -------------------------------------------------------------------------------------------------
 
