3d3515e98a177ec3c68e12732b1d321ab3fe6356
S. Gokhan Topcu <gtopcu@gmail.com>
2025-07-22 19:49:08 +0300

    github guard

--- Serverless/utils_python/_pplay1.py
+++ Serverless/utils_python/_pplay1.py
@@ -632,6 +632,7 @@ from numbers import Number # isinstance(x, Number)
 # -------------------------------------------------------------------------------------------------
 
 # import orjson
+# orjson.loads(body)
 
 # with open("file.json", "r") as f:
 #     data:dict[str, Any] = json.load(f)  # json.dump(f)
